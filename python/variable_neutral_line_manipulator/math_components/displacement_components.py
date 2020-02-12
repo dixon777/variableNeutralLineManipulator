2208407c1bdf3d80ca22b0f83aeddc7d77f5cf68
dixon777 <dickson0717@hotmail.com>
2020-02-12 22:21:58 +1100

    simplify displacement components

--- python/variable_neutral_line_manipulator/math_components/displacement_components.py
+++ python/variable_neutral_line_manipulator/math_components/displacement_components.py
@@ -5,22 +5,22 @@ Based on the frame aligned with the bottom curvature orientation
 (Bottom curvature rotates about the x-axis of the frame)
 """
 
-
-def evalDispBetweenCenters(curveRadius:float, ringLength:float, isTop:bool):
-    """
-        Evaluate 
-    """
-    return ringLength/2 - curveRadius if isTop else curveRadius - ringLength/2
+# Used as a reference, commented out due to simplicity
+# def evalDispBetweenCenters(curveRadius:float, ringLength:float, isTop:bool):
+#     """
+#         Evaluate 
+#     """
+#     return ringLength/2 - curveRadius if isTop else curveRadius - ringLength/2
     
 
-# def evalHorizontalDispFromCableToAxis(CurveRadius: float, 
-#                                 curvatureSpanAngle: float):
-#     return CurveRadius*math.sin(curvatureSpanAngle/2)
-
+# Not required for model with tendon in the cable guide removed
 def evalKnobDisp(ringLength: float, 
                 knobLength: float,
                 horizontalDispFromCableToAxis: float,
                 cableOrientationRF: float) -> np.ndarray:
+    """
+        Evaluate displacement from ring center to knob
+    """
     return np.array((
         horizontalDispFromCableToAxis*math.cos(cableOrientationRF),
         horizontalDispFromCableToAxis*math.sin(cableOrientationRF),
@@ -31,15 +31,15 @@ def evalTopGuideEndDisp(ringLength: float,
                         topCurveRadius: float,
                         horizontalDispFromCableToAxis: float,
                         cableOrientationRF: float,
-                        topOrientationRF: float)-> np.ndarray:
-
+                        topOrientationRF: float) -> np.ndarray:
+    """
+        Evaluate displacement from ring center to any top end of tendon guide
+    """
     horizontalDispAlongCurve = horizontalDispFromCableToAxis*math.sin(cableOrientationRF - topOrientationRF)
     return np.array((
         horizontalDispFromCableToAxis*math.cos(cableOrientationRF),
         horizontalDispFromCableToAxis*math.sin(cableOrientationRF),
-        math.sqrt(topCurveRadius**2 - horizontalDispAlongCurve**2) + evalDispBetweenCenters(curveRadius=topCurveRadius, 
-                                                                                            ringLength=ringLength, 
-                                                                                            isTop=True)
+        math.sqrt(topCurveRadius**2 - horizontalDispAlongCurve**2) + ringLength/2 - topCurveRadius
     ))
 
    
@@ -48,14 +48,14 @@ def evalBottomGuideEndDisp(ringLength: float,
                             bottomCurveRadius: float,
                             horizontalDispFromCableToAxis: float,
                             cableOrientationRF: float)-> np.ndarray:
-
+    """
+        Evaluate displacement from ring center to any bottom end of tendon guide
+    """
     horizontalDispAlongCurve = horizontalDispFromCableToAxis*math.sin(cableOrientationRF)
     return np.array((
         horizontalDispFromCableToAxis*math.cos(cableOrientationRF),
         horizontalDispFromCableToAxis*math.sin(cableOrientationRF),
-        -math.sqrt(bottomCurveRadius**2 - horizontalDispAlongCurve**2) + evalDispBetweenCenters(curveRadius=bottomCurveRadius, 
-                                                                                                ringLength=ringLength, 
-                                                                                                isTop=False)
+        -math.sqrt(bottomCurveRadius**2 - horizontalDispAlongCurve**2) + bottomCurveRadius - ringLength/2
     ))
     
     
@@ -63,26 +63,27 @@ def evalTopContactDisp(ringLength: float,
                     topCurveRadius: float,
                     topJointAngle: float, 
                     topOrientationRF: float):
+    """
+        Evaluate displacement from ring center to center of top contacting line
+    """
     halfJointAngle = topJointAngle/2
     return np.array((
         topCurveRadius*math.sin(halfJointAngle)*math.sin(topOrientationRF),
         -topCurveRadius*math.sin(halfJointAngle)*math.cos(topOrientationRF),
-        topCurveRadius*math.cos(halfJointAngle) + evalDispBetweenCenters(curveRadius=topCurveRadius, 
-                                                                        ringLength=ringLength, 
-                                                                        isTop=True)
+        topCurveRadius*math.cos(halfJointAngle) + ringLength/2 - topCurveRadius
     ))
 
 def evalBottomContactDisp(ringLength: float,
                         bottomCurveRadius: float,
                         bottomJointAngle: float):
-    
+    """
+        Evaluate displacement from ring center to center of bottom contacting line
+    """
     halfJointAngle = bottomJointAngle/2
     return np.array((
         0,
         -bottomCurveRadius*math.sin(halfJointAngle),
-        -bottomCurveRadius*math.cos(halfJointAngle) + evalDispBetweenCenters(curveRadius=bottomCurveRadius, 
-                                                                            ringLength=ringLength, 
-                                                                            isTop=False)
+        -bottomCurveRadius*math.cos(halfJointAngle) + bottomCurveRadius - ringLength/2
     ))
 
     
