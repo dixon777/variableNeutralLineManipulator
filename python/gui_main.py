8994de143559a1fb00505cdac6f71ce088113dfe
dixon777 <dickson0717@hotmail.com>
2020-05-30 11:31:17 +0800

    fix external python modules dependencies

--- python/gui_main.py
+++ python/gui_main.py
@@ -1,9 +1,9 @@
-from variable_neutral_line_manipulator.display.gui import App
-from variable_neutral_line_manipulator.common import Logger
-import numpy as np
-np.set_printoptions(3, suppress =True)
-import logging
-Logger.get().setLevel(logging.INFO)
+from variable_neutral_line_manipulator import App
+# from variable_neutral_line_manipulator.common import Logger
+# import numpy as np
+# np.set_printoptions(3, suppress =True)
+# import logging
+# Logger.get().setLevel(logging.INFO)
     
 def main():
     App.run()
